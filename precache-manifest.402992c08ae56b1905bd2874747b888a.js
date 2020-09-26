df642c4a1ca4b64390545715f3f1431a468cc7aa
Mostafa Kamal <=>
2020-09-26 22:35:27 +0600

    Updates

---
+++ precache-manifest.402992c08ae56b1905bd2874747b888a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cc5325d064061b4d414dd3bfc9b289",
+    "url": "/resort/index.html"
+  },
+  {
+    "revision": "1d8c0079070abfa9a166",
+    "url": "/resort/static/css/main.1f5051cd.chunk.css"
+  },
+  {
+    "revision": "929370b0926a19fc39e1",
+    "url": "/resort/static/js/2.b6f4ab4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resort/static/js/2.b6f4ab4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8c0079070abfa9a166",
+    "url": "/resort/static/js/main.1da19b9b.chunk.js"
+  },
+  {
+    "revision": "d126f1492c1ad25eb3d4",
+    "url": "/resort/static/js/runtime-main.1f9fe477.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
