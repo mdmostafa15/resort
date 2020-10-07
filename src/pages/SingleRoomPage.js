ae6afe4f4112ec4e7ca382eff29b587516d35b73
Mostafa Kamal <=>
2020-10-07 11:29:31 +0600

    single room page is completed

--- src/pages/SingleRoomPage.js
+++ src/pages/SingleRoomPage.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
 import defaultBgImg from "../images/room-1.jpeg";
-import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import StyleSingleRoomHero from "../components/StyleSingleRoomHero";
 
@@ -26,7 +25,7 @@ class SingleRoomPage extends Component {
     if (!room) {
       return (
         <div className="error">
-          <h3>No Such Room is Fund...</h3>
+          <h3>no ruch room could be fund...</h3>
           <Link to="/resort/rooms" className="btn btn-primary">
             back to rooms
           </Link>
@@ -36,28 +35,59 @@ class SingleRoomPage extends Component {
     // destructure room
     const {
       name,
-      slug,
-      type,
       price,
       size,
       capacity,
       pets,
       breakfast,
-      featured,
       description,
       extras,
       images,
     } = room;
+    const [heroImg, ...restOfImages] = images;
+    console.log("rest of images", restOfImages);
     return (
-      <div>
-        <StyleSingleRoomHero img={images[0] || defaultBgImg}>
+      <>
+        <StyleSingleRoomHero img={heroImg || defaultBgImg}>
           <Banner title={`${name} room`}>
             <Link to="/resort/rooms" className="btn btn-primary">
               back to rooms
             </Link>
           </Banner>
         </StyleSingleRoomHero>
-      </div>
+        <section className="single-room">
+          <div className="single-room-images">
+            {restOfImages.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: {size} sqft</h6>
+              <h6>
+                max capacity:{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>pets: {pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((extra, index) => {
+              return <li key={index}>-{extra}</li>;
+            })}
+          </ul>
+        </section>
+      </>
     );
   }
 }
